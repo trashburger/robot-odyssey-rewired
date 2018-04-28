c65c7ded231d083d003208a57857bc029db49972
Brian Myers <brian.carl.myers@gmail.com>
2018-04-28 14:25:24 -0500

    refactored build.rs to use generics, slices, and &strs instead of Vectors and Strings

--- ro-engine/build.rs
+++ ro-engine/build.rs
@@ -1,9 +1,22 @@
-use std::process::Command;
 use std::env;
+use std::ffi::OsStr;
+use std::fmt::Debug;
+use std::process::Command;
 
-fn python(args: &Vec<String>) {
-    let status = Command::new("python3").args(args).status();
-    if !status.expect("Failed to execute python subprocess").success() {
+const GAME_SCRIPTS: [&str; 5] = [
+    "bt_game.py",
+    "bt_lab.py",
+    "bt_tutorial.py",
+    "bt_menu.py",
+    "bt_renderer.py",
+];
+
+fn python<S: AsRef<OsStr> + Debug>(args: &[S]) {
+    let status = Command::new("python3")
+        .args(args)
+        .status()
+        .expect("Failed to execute python subprocess");
+    if !status.success() {
         panic!("Error from Python process while running {:?}", args);
     }
 }
@@ -12,15 +25,10 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let original_dir = env::var("ORIGINAL_DIR").unwrap_or("../original".to_string());
 
-    python(&vec!["scripts/check-originals.py".to_string(),
-            original_dir.clone(), out_dir.clone()]);
+    python(&["scripts/check-originals.py", &original_dir, &out_dir]);
 
-    let game_scripts = [
-        "bt_game.py", "bt_lab.py", "bt_tutorial.py",
-        "bt_menu.py", "bt_renderer.py"
-    ];
-
-    for script in game_scripts.iter() {
-        python(&vec![format!("scripts/{}", script), out_dir.clone()]);
+    for script in GAME_SCRIPTS.iter() {
+        let script = format!("scripts/{}", script);
+        python(&[&script, &out_dir]);
     }
 }
