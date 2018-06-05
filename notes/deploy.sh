af80c5f6c79d9dd5437c7752bf27e53bd3cdc46c
Micah Elizabeth Scott <micah@misc.name>
2018-06-04 17:04:42 -0700

    rsync is nice
    Worried about non-atomic overwrites

--- notes/deploy.sh
+++ notes/deploy.sh
@@ -5,5 +5,5 @@ git submodule update --init --recursive
 npm install
 make clean
 make
-scp `ls dist/* | grep -v \.html$; ls dist/index.html` robotodyssey.online:~/robotodyssey.online/
-scp notes/htaccess robotodyssey.online:~/robotodyssey.online/.htaccess
+rsync -avz `ls dist/* | grep -v \.html$` robotodyssey.online:~/robotodyssey.online/
+rsync -avz dist/*.html robotodyssey.online:~/robotodyssey.online/
