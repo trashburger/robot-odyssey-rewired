27708b88b2974aa7e2d8207ad8e3eb0edfcd96ca
Micah Elizabeth Scott <micah@misc.name>
2018-06-09 13:10:23 -0700

    SHW decoder: faster, smaller output

--- notes/showfile.py
+++ notes/showfile.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# pip3 install Pillow
 
 import os, struct
 from PIL import Image
@@ -6,12 +7,12 @@ from PIL import Image
 
 def decode_next_image(file):
     # 9B: End of frame
-    # 1B: 8-bit count, 8-bit value to repeat 
+    # 1B: 8-bit count, 8-bit value to repeat
     # E4: 16-bit LE count, 8-bit value to repeat
     # E6: 16-bit dest address
     # Other bytes are literal
 
-    img = Image.new('RGBA', (320, 200))
+    pixels = bytearray(320*200)
     ptr = 0
 
     while True:
@@ -23,7 +24,16 @@ def decode_next_image(file):
         byte = ord(byte)
 
         if byte == 0x9B:
-            # Successful end of image
+            # Successful end of image, wrap the 8bpp buffer in a PIL image.
+
+            img = Image.frombuffer('P', (320, 200), pixels, 'raw', 'P', 0, 1)
+            img.putpalette([
+                0, 0, 0,
+                0, 0, 0,
+                0x09, 0xA9, 0xD8,
+                0xED, 0x7A, 0x31,
+                0xFF, 0xFF, 0xFF,
+            ])
             return img
 
         if ptr >= 0x4000:
@@ -32,58 +42,54 @@ def decode_next_image(file):
 
         if byte == 0x1B:
             # 8-bit RLE
-            count = struct.unpack('<B', file.read(1))[0]
-            byte = ord(file.read(1))
+            count, byte = struct.unpack('<BB', file.read(2))
 
         elif byte == 0xE4:
             # 16-bit RLE
-            count = struct.unpack('<H', file.read(2))[0]
-            byte = ord(file.read(1))
+            count, byte = struct.unpack('<HB', file.read(3))
 
         elif byte == 0xE6:
             # 16-bit dest pointer
             ptr = struct.unpack('<H', file.read(2))[0]
             continue
 
-        # A run of identical bytes, each with 4 pixels
+        # A run of identical bytes, each with 4 pixels.
+        # Each CGA byte-wide write turns into four 8bpp pixels.
+
         while count > 0:
-            plot_cga_byte(img, ptr, byte)
+
+            # Convert CGA framebuffer address 'ptr' to 8bpp byte address.
+            # CGA is arranged as two 0x2000-byte interleaved planes.
+            if ptr >= 0x2000:
+                pixaddr = (4 * ((ptr - 0x2000) % 80)) + 320*(2 * ((ptr - 0x2000) // 80) + 1)
+            else:
+                pixaddr = (4 * (ptr % 80)) + 320*(2 * (ptr // 80))
+
+            if pixaddr <= 63996:
+                # Four pixels per byte, MSB first. Palette entry zero is transparent
+                # for pixels that aren't written, and the next 4 entries represent CGA colors.
+                pixels[pixaddr] = 1 + (3 & (byte >> 6))
+                pixels[pixaddr + 1] = 1 + (3 & (byte >> 4))
+                pixels[pixaddr + 2] = 1 + (3 & (byte >> 2))
+                pixels[pixaddr + 3] = 1 + (3 & byte)
+
             ptr += 1
             count -= 1
-
-
-def plot_cga_byte(img, ptr, byte):
-    palette = [
-        0xff000000,
-        0xffd8a909,
-        0xff317aed,
-        0xffffffff,
-    ]
-
-    if ptr >= 0x2000:
-        x = 4 * ((ptr - 0x2000) % 80)
-        y = 2 * ((ptr - 0x2000) // 80) + 1
-    else:
-        x = 4 * (ptr % 80)
-        y = 2 * (ptr // 80)
-
-    for bit in range(4):
-        img.paste(palette[3 & (byte >> 6)], (x,y,x+1,y+1))
-        img.paste(palette[3 & (byte >> 4)], (x+1,y,x+2,y+1))
-        img.paste(palette[3 & (byte >> 2)], (x+2,y,x+3,y+1))
-        img.paste(palette[3 & (byte     )], (x+3,y,x+4,y+1))
 
 
 def decode_all_images_from_show(filename):
     number = 0
     with open(filename, 'rb') as file:
         while True:
+            first_offset = file.tell()
             img = decode_next_image(file)
+            file_bytes = file.tell() - first_offset
             if img:
                 number += 1
                 name = "tmp/%s-%d.png" % (os.path.basename(filename), number)
-                img.save(name)
-                print(name)
+                img.save(name, transparency=0, optimize=1)
+                print("%s was %d bytes compressed, %d byte PNG" %
+                    (name, file_bytes, os.stat(name).st_size))
             else:
                 break
 
