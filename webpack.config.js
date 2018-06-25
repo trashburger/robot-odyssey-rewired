475ea8d9c2c04537756b12504d2b47b1b4eafdc4
Micah Elizabeth Scott <micah@misc.name>
2018-06-25 14:22:16 -0700

    Webpack bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const RemoveAssetsPlugin = require('remove-assets-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const title = 'Robot Odyssey Rewired';
 
@@ -37,6 +38,9 @@ module.exports = {
         hints: false,
     },
     plugins: [
+
+        // CSS and HTML inlined
+
         new MiniCssExtractPlugin({
             filename: '[name].[hash].css'
         }),
@@ -50,6 +54,20 @@ module.exports = {
         }),
         new HtmlWebpackInlineSourcePlugin(),
         new RemoveAssetsPlugin(/\.css$/),
+
+        // Generate the webfont from our pile of SVGs extracted from the game font
+
+        new WebfontPlugin({
+            files: './build/font/glyph-*.svg',
+            dest: './build/font/',
+            fontName: 'rofont',
+            formats: ['woff'],
+            template: './src/assets/font.template.css',
+            fixedWidth: true,
+            startUnicode: 0x20,
+        }),
+
+        // Generate content for the progressive web app
 
         new FaviconsWebpackPlugin({
             logo: path.resolve('./build/show/icon.png'),
@@ -65,15 +83,6 @@ module.exports = {
                 firefox: false,         // No PWA manifest; that's below
             }
         }),
-        new WebfontPlugin({
-            files: './build/font/glyph-*.svg',
-            dest: './build/font/',
-            fontName: 'rofont',
-            formats: ['woff'],
-            template: './src/assets/font.template.css',
-            fixedWidth: true,
-            startUnicode: 0x20,
-        }),
         new WebpackPwaManifest({
             name: title,
             background_color: '#000000',
@@ -85,8 +94,17 @@ module.exports = {
             ],
         }),
 
-        // Offline plugin should be last
+        // Offline plugin must be AFTER all resources we want to cache.
         new OfflinePlugin(),
+
+        // Analyze resource sizes.
+        // By putting this after OfflinePlugin it stays out of our sw.js bundle,
+        // and put the output in our build dir rather than 'dist' so it isn't uploaded.
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            openAnalyzer: false,
+            reportFilename: '../build/bundle-analyzer-report.html',
+        }),
     ],
     module: {
         defaultRules: [
