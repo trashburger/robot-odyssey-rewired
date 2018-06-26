920b4317047fcfd7834c05862b93d8c84ca89eee
Micah Elizabeth Scott <micah@misc.name>
2018-06-26 11:23:40 -0700

    formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,19 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': false
     },
-    "extends": "eslint:recommended",
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "sourceType": "module"
+    'extends': 'eslint:recommended',
+    'parser': 'babel-eslint',
+    'parserOptions': {
+        'sourceType': 'module'
     },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "eqeqeq": [
-            "error",
-            "always"
-        ]
+    'rules': {
+        'indent': [ 'error', 4 ],
+        'linebreak-style': [ 'error', 'unix' ],
+        'quotes': [ 'error', 'single' ],
+        'semi': [ 'error', 'always' ],
+        'eqeqeq': [ 'error', 'always' ],
     }
 };
