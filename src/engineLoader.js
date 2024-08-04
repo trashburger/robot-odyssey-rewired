e8582b9288e75ad339a222138b253c7b0d3a6b6f
Micah Elizabeth Scott <micah@scanlime.org>
2024-08-04 13:58:43 -0700

    Emscripten modules do return a Promise now
    It used to be the module constructor returned the module object itself
    which also served as a promise-like object and as the constructor
    options. We've upgraded to a version of emscripten that uses a real
    Promise, so simplify the engineLoader accordingly.

--- src/engineLoader.js
+++ src/engineLoader.js
@@ -8,30 +8,8 @@ export var complete = null;
 
 export function init()
 {
-    complete = new Promise((resolve, reject) => {
-
-        // The engine is NOT a promise, but if the real promise implementation
-        // thinks we're returning a promise it can cause an infinite loop.
-        //
-        // We don't want anyone using the fake then() implementation from emscripten,
-        // so destroy that here and prevent this resolve() from interpreting the
-        // instance value as a promise.
-
-        const done = () => {
-            delete instance.then;
-            resolve(instance);
-        };
-
-        const options = {
-            locateFile: () => EngineWasm,
-            onAbort: reject,
-            onRuntimeInitialized: done,
-        };
-
-        try {
-            instance = EngineFactory(options);
-        } catch (e) {
-            reject(e);
-        }
-    });
+    instance = {
+        locateFile: () => EngineWasm,
+    };
+    complete = EngineFactory(instance);
 }
