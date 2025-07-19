508cda57cb660985ec5d638b56486a43236fbfe9
Micah Elizabeth Scott <micah@scanlime.org>
2025-07-18 19:13:19 -0700

    docker-build: copy out build directory and package-lock

--- notes/docker-build.sh
+++ notes/docker-build.sh
@@ -6,5 +6,7 @@ docker build .
 image=$(docker build -q .)
 container=$(docker create $image)
 docker cp $container:/work/dist dist
+docker cp $container:/work/build build
+docker cp $container:/work/package-lock.json ./
 docker rm $container
 
