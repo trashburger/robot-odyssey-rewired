d711979213ab4144f35e61b257935c22586d0781
Micah Elizabeth Scott <micah@scanlime.org>
2025-07-19 13:40:47 -0700

    Optimization hints for inner parts of RGBA drawing
    - Marked pixel plotters as always_inline (using sbt86's macro)
    - Full loop unrolling for zoomed pixels and for sprite/tile rows

--- src/engine/draw.cpp
+++ src/engine/draw.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include "draw.h"
 #include "roData.h"
+#include "sbt86.h"
 
 RGBDraw::RGBDraw(ColorTable &colorTable)
     : colorTable(colorTable)
@@ -10,32 +11,34 @@ RGBDraw::RGBDraw(ColorTable &colorTable)
     memset(backbuffer, 0, sizeof backbuffer);
 }
 
-void RGBDraw::pixel_160x192(unsigned x, unsigned y, uint8_t color, unsigned anchor_x, unsigned anchor_y)
+SBT_INLINE void RGBDraw::pixel_160x192(unsigned x, unsigned y, uint8_t color, unsigned anchor_x, unsigned anchor_y)
 {
     pixel_320x192(2*x, y, color, 2*anchor_x, anchor_y);
     pixel_320x192(2*x+1, y, color, 2*anchor_x + 1, anchor_y);
 }
 
-void RGBDraw::pixel_320x192(unsigned x, unsigned y, uint8_t color, unsigned anchor_x, unsigned anchor_y)
+SBT_INLINE void RGBDraw::pixel_320x192(unsigned x, unsigned y, uint8_t color, unsigned anchor_x, unsigned anchor_y)
 {
     if (x >= 320 || y >= 192) {
         return;
     }
 
-    unsigned tile_size = ColorTable::SCREEN_TILE_SIZE;
-    unsigned zoom = CGAFramebuffer::ZOOM;
-    unsigned screen_x = x * zoom;
-    unsigned screen_y = (191 - y) * zoom;
+    const unsigned tile_size = ColorTable::SCREEN_TILE_SIZE;
+    const unsigned zoom = CGAFramebuffer::ZOOM;
+    const unsigned screen_x = x * zoom;
+    const unsigned screen_y = (191 - y) * zoom;
 
-    uint32_t *pattern = colorTable.patterns + (tile_size * tile_size * color);
-    unsigned pattern_x = anchor_x * zoom;
-    unsigned pattern_y = tile_size - (1 + anchor_y) * zoom;
+    const uint32_t *pattern = colorTable.patterns + (tile_size * tile_size * color);
+    const unsigned pattern_x = anchor_x * zoom;
+    const unsigned pattern_y = tile_size - (1 + anchor_y) * zoom;
 
+    #pragma unroll
     for (unsigned zy = 0; zy < zoom; zy++) {
+        #pragma unroll
         for (unsigned zx = 0; zx < zoom; zx++) {
-            unsigned px = (pattern_x + zx) % tile_size;
-            unsigned py = (pattern_y + zy) % tile_size;
-            uint32_t rgb = pattern[px + py * tile_size];
+            const unsigned px = (pattern_x + zx) % tile_size;
+            const unsigned py = (pattern_y + zy) % tile_size;
+            const uint32_t rgb = pattern[px + py * tile_size];
             backbuffer[screen_x + zx + (zy + screen_y) * SCREEN_WIDTH] = rgb;
         }
     }
@@ -44,10 +47,10 @@ void RGBDraw::pixel_320x192(unsigned x, unsigned y, uint8_t color, unsigned anch
 void RGBDraw::sprite(uint8_t *data, uint8_t x, uint8_t y, uint8_t color)
 {
     for (unsigned byte_index = 0; byte_index < 16; byte_index++) {
-        uint8_t byte = data[byte_index];
+        const uint8_t byte = data[byte_index];
         for (unsigned bit_index = 0; bit_index < 7; bit_index++) {
             if ((byte >> bit_index) & 1) {
-                unsigned bitx = 6 - bit_index;
+                const unsigned bitx = 6 - bit_index;
                 pixel_160x192(x + bitx, y + byte_index, color, bitx, byte_index);
             }
         }
@@ -56,23 +59,22 @@ void RGBDraw::sprite(uint8_t *data, uint8_t x, uint8_t y, uint8_t color)
 
 void RGBDraw::playfield(uint8_t *data, uint8_t foreground, uint8_t background)
 {
-    // Profiler notes: About 30% of CPU is spent in this function!
-
-    unsigned tile_size = ColorTable::SCREEN_TILE_SIZE;
+    const unsigned tile_size = ColorTable::SCREEN_TILE_SIZE;
 
     for (unsigned byte_index = 0; byte_index < 30; byte_index++) {
-        uint8_t byte = data[byte_index];
+        const uint8_t byte = data[byte_index];
         for (unsigned bit_index = 0; bit_index < 8; bit_index++) {
-            unsigned pattern_id = ((byte >> bit_index) & 1) ? foreground : background;
-            uint32_t *pattern = colorTable.patterns + (tile_size * tile_size * pattern_id);
-            unsigned tile_x = (byte_index % 10)*2 + (bit_index >> 2);
-            unsigned tile_y = (byte_index / 10)*4 + (bit_index & 3);
-            unsigned screen_x = tile_x * tile_size;
-            unsigned screen_y = tile_y * tile_size;
+            const unsigned pattern_id = ((byte >> bit_index) & 1) ? foreground : background;
+            const uint32_t *pattern = colorTable.patterns + (tile_size * tile_size * pattern_id);
+            const unsigned tile_x = (byte_index % 10)*2 + (bit_index >> 2);
+            const unsigned tile_y = (byte_index / 10)*4 + (bit_index & 3);
+            const unsigned screen_x = tile_x * tile_size;
+            const unsigned screen_y = tile_y * tile_size;
             uint32_t *dest = backbuffer + screen_x + screen_y * SCREEN_WIDTH;
             for (unsigned y = 0; y < tile_size; y++) {
-                uint32_t *pattern_line = pattern + y*tile_size;
+                const uint32_t *pattern_line = pattern + y*tile_size;
                 uint32_t *dest_line = dest + y*SCREEN_WIDTH;
+                #pragma unroll
                 for (unsigned x = 0; x < tile_size; x++) {
                     dest_line[x] = pattern_line[x];
                 }
@@ -113,6 +115,7 @@ void RGBDraw::text(uint8_t *string, uint8_t *font_data, uint8_t x, uint8_t y,
                 uint8_t *font = font_start + (c - 0x20);
                 for (unsigned line = 0; line < 8; line++) {
                     uint8_t byte = font[line * 0x60];
+                    #pragma unroll
                     for (unsigned bit = 0; bit < 8; bit++) {
                         if ((byte << bit) & 0x80) {
                             if (style == RO_TEXT_BIG) {
