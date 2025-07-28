36dfb458bd742a985be4889b4547562450311789
Micah Elizabeth Scott <micah@scanlime.org>
2025-07-27 21:35:36 -0700

    Clean up ghidra_scripts
    The small single-purpose scripts have all been combined into RoRealmodeFixups, just keep that one.

--- notes/RoRealmodeFixups.py
+++ notes/RoRealmodeFixups.py
@@ -1,14 +1,46 @@
-# Fix segment addressing in 16-bit real mode
-#@author Micah E Scott
+# Ghidra command to apply hacky real-mode fixups to the current selection.
+#@author Micah 
 #@category RealMode
-#@keybinding 
-#@menupath
+#@keybinding
+#@menupath Tools/RoRealmodeFixups
 #@toolbar 
 
+# Fix near call/jmp in 16-bit real mode
+
+from ghidra.program.model.mem import MemoryAccessException
+from ghidra.program.model.symbol import FlowType
+from ghidra.program.model.listing import FlowOverride
 from ghidra.program.model.scalar import Scalar
 from ghidra.program.model.lang import OperandType, Register
 from ghidra.program.model.symbol import RefTypeFactory
 from ghidra.program.model.listing import Instruction
+
+for unit in currentProgram.getListing().getCodeUnits(currentSelection, True):
+    try:
+        if unit.getLength() == 3 and unit.getBytes()[0]&0xFF in [0xe8, 0xe9]:
+            addr = unit.getMinAddress()
+            segment = addr.getSegment()
+            next = addr.getSegmentOffset() + unit.getLength()
+            target_offset = (next + ((unit.getBytes()[2]&0xFF)<<8) + (unit.getBytes()[1]&0xFF))&0xFFFF
+            target_addr = addr.getAddressSpace().getAddress(segment, target_offset)
+
+            for old_ref in unit.getReferencesFrom():
+                print(old_ref)
+                if True:
+                    removeReference(old_ref)
+
+            print(unit, target_addr)
+            if True:
+                createMemoryReference(unit, 0, target_addr, {
+                    0xe8: FlowType.UNCONDITIONAL_CALL,
+                    0xe9: FlowType.UNCONDITIONAL_JUMP
+                }[unit.getBytes()[0]&0xFF])
+                unit.setFlowOverride(FlowOverride.NONE)
+
+    except MemoryAccessException:
+        pass
+
+# Fix segment addressing in 16-bit real mode
 
 cs = currentProgram.getLanguage().getRegister("cs")
 ds = currentProgram.getLanguage().getRegister("ds")
